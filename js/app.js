63fcd8573eeb1b37655fcb2366580e6bbb09392d
cjescalona1 <cjescalona1@gmail.com>
2022-03-27 00:44:21 -0400

    burguer menu

--- js/app.js
+++ js/app.js
@@ -57,3 +57,8 @@ function lazyLoad() {
 	
 }
 
+function menuOnClick() {
+	document.getElementById("menu-bar").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+  }
